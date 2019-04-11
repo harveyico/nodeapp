3593ab1b7ac123dd0eb7d908d511fedd236357af
Harvey Ico <godstrikerharvey@gmail.com>
2019-04-11 11:46:50 +0900

    Add mongodb for database

--- src/app.js
+++ src/app.js
@@ -9,12 +9,17 @@ const feathers = require('@feathersjs/feathers');
 const configuration = require('@feathersjs/configuration');
 const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
-
+const errors = require('@feathersjs/errors');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb')
+const service = require('feathers-mongodb');
 
 const middleware = require('./middleware');
 const services = require('./services');
 const appHooks = require('./app.hooks');
 const channels = require('./channels');
+
+const mongodb = require('./mongodb');
 
 const app = express(feathers());
 
@@ -34,6 +39,8 @@ app.use('/', express.static(app.get('public')));
 app.configure(express.rest());
 app.configure(socketio());
 
+app.configure(mongodb);
+
 // Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
 // Set up our services (see `services/index.js`)
@@ -46,5 +53,31 @@ app.use(express.notFound());
 app.use(express.errorHandler({ logger }));
 
 app.hooks(appHooks);
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Connect to mongodb
+MongoClient.connect('mongodb://localhost:27017/nodeapp').then(function(db){
+  // Connect to the db, create and register a Feathers service.
+  app.use('/messages', service({
+    Model: db.collection('messages'),
+    paginate: {
+      default: 2,
+      max: 4
+    }
+  }));
+
+  // A basic error handler, just like Express
+  app.use(errors.handler());
+
+  // Start the server
+  const server = app.listen(3030);
+  server.on('listening', function() {
+    console.log('Feathers Message MongoDB service running on 127.0.0.1:3030');
+  });
+}).catch(function(error){
+  console.error(error);
+});
 
 module.exports = app;
