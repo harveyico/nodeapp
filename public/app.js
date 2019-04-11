a40c59c81e0a230eb99379732f0cd308fa678d7a
Harvey Ico <godstrikerharvey@gmail.com>
2019-04-11 15:40:53 +0900

    Fix uneeded scripts

--- public/app.js
+++ public/app.js
@@ -9,10 +9,4 @@ client.configure(feathers.socketio(socket));
 client.configure(feathers.authentication({
   storage: window.localStorage
 }));
-
-// Listen to created events and add the new message in real-time
-client.service('messages').on('created', addMessage);
-// We will also see when new users get created in real-time
-client.service('users').on('created', addUser);
-
-login();
+console.log('test')
